65b18ef1e1d014848bc85e492b77bc691cbd3cd1
Jim Peters <jimjitsuesq@gmail.com>
2021-09-12 13:39:20 -0700

    Standardized image names

--- src/Components/Projects/RightSidebar.js
+++ src/Components/Projects/RightSidebar.js
@@ -7,7 +7,7 @@ function RightSidebar (props) {
   const screenshotsArray = [];
   function generateScreenshotsArray () {
     for (let i = 0; i < currentProject.screenshots; i++) {
-      screenshotsArray.push(require(`../../images/projects/${currentProject.project_name_path}/thumbnail${i+1}.JPG`)) 
+      screenshotsArray.push(require(`../../images/projects/${currentProject.project_name_path}/thumbnail${i+1}.jpg`)) 
     }
   }
   generateScreenshotsArray();
